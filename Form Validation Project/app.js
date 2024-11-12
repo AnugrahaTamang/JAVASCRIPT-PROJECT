c0d6cc56cef31ce8bab57d56426516b3752a9a0f
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-11-12 16:12:40 +0545

    form validation

---
+++ Form Validation Project/app.js
@@ -0,0 +1,9 @@
+const name = document.querySelector("#name")
+const email = document.querySelector("#email")
+const password = document.querySelector("#password")
+const confirmpassword = document.querySelector("#confirmpassword")
+const submit = document.querySelector("#submit")
+submit.addEventListener("click", (e) => {
+   e.preventDefault();
+   
+})
